49976e56daf377e389a5f0c471df535753aa5e19
hoaiphuc2000 <lephanhoaiphuc@gmail.com>
2022-04-04 01:51:46 +0700

    test

---
+++ src/components/Test/Test.js
@@ -0,0 +1,14 @@
+import bgimg from "assets/background-05.png"
+import "./Test.css"
+
+const test = () => {
+    return (
+        <div className="background">
+            <div className="background1">
+                <div className="left">Tôi là Đỗ Thị Kim Liên, sinh ra tại Mê Linh, Vĩnh Phúc. Xuất thân trong một gia đình có truyền thống làm giáo dục, tôi được định hướng trở thành một cô giáo ngay khi còn nhỏ. Sau khi tốt nghiệp Khoa Ngữ văn của Trường Đại học Sư phạm Hà Nội II, tôi đứng lớp giảng dạy 3 năm, rồi quyết tâm chia tay nghề để vào Nam lập nghiệp. Với hơn 30 năm kinh nghiệm trong lĩnh vực tài chính, điều hành và hoạt động vì cộng đồng, tôi luôn mong muốn được làm việc với những nhà sáng lập và khởi nghiệp trẻ với tư cách vừa là nhà đầu tư chiến lược, vừa là người cố vấn. </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    );
+  };
+  export default test;
