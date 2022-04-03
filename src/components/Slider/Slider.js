2ca63be7b435202adc14741407dcbcdceb5e8abd
hoaiphuc2000 <lephanhoaiphuc@gmail.com>
2022-04-04 00:18:18 +0700

    Add Carousel top, need to adjust for mobile responsive

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,18 @@
+import "antd/dist/antd.css";
+import { Carousel } from "antd";
+import BannerImage1 from "assets/banner1.png";
+import BannerImage2 from "assets/banner2.png";
+import "./Slider.css"
+
+const LIST_IMG = [BannerImage1,BannerImage2];
+
+const Phuc = () => {
+  return (
+      <Carousel autoplay >
+        {LIST_IMG.map((item, idx) => (
+          <div className="outer"><img src={item} key={idx} className="innerImage"/></div>
+        ))}
+      </Carousel>
+  );
+};
+export default Phuc;
