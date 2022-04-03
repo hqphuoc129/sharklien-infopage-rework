0a8462964d3b1bcc5998d49f3cb7e190dd85c03d
owentr1369 <tamvaa1306@gmail.com>
2022-04-03 22:25:06 +0700

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import FooterLogo from '../../assets/logo.png'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div  className="footer_logo">
+        <div className ='footer_logo-wrapper'> 
+          <img src = {FooterLogo}/> 
+        </div> 
+      </div>
+        <div className='footer_text-wrapper'>
+        <p className="footer_text">
+        http://sharklien.vn/index.html
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
