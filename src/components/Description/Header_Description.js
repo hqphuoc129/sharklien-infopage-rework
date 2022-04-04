9f3f954f9f96ba416ff54632956863ee15bf0fd8
Tấn Phát Võ <recca5p@gmail.com>
2022-04-04 22:31:48 +0700

    - BiographyPage: finished

---
+++ src/components/Description/Header_Description.js
@@ -0,0 +1,11 @@
+const HeaderDescription = props => {
+    return ( <
+        div >
+        <
+        h2 style = {
+            { textAlign: "center", paddingTop: "5%", fontSize: "36pt" } } > { props.title } < /h2> <
+        /div>
+    )
+}
+
+export default HeaderDescription;
