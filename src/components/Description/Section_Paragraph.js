46f3fcaadf66a768b011ab6488a557ace161420c
Tấn Phát Võ <recca5p@gmail.com>
2022-04-05 19:41:52 +0700

    -fix: paragraph component (Biography) -MediaPage: finished

--- src/components/Description/Section_Paragraph.js
+++ src/components/Description/Section_Paragraph.js
@@ -1,7 +1,6 @@
 const Section_Paragraph = props => {
-    return ( <
-        p style = {
-            { textAlign: "justify", marginLeft: "10%", marginRight: "10%", marginBottom: "5%", fontSize: "21pt" } } > { props.content } < /p>
+    return ( 
+    <p style = {{ textAlign: "justify", marginLeft: "10%", marginRight: "10%", marginBottom: "5%", fontSize: "21pt", lineHeight: "1.5" } } > { props.content } </p>
     )
 }
 
