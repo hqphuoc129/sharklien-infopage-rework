9287a02bd03e10a881cd70e0be9e1625486e9b8a
Tấn Phát Võ <recca5p@gmail.com>
2022-04-07 22:01:06 +0700

    fix MediaPage: responsive fix BiographyPage: responsive

--- src/components/Media/CustomerSlider.js
+++ src/components/Media/CustomerSlider.js
@@ -9,7 +9,7 @@ const CustomSlider = (props) => {
           return (
             <div >
             <a >
-              <img  style={{width: "7.469rem",height:"5.406rem", objectFit: "cover"}} src={require(`../../views/Media/assets/${i + 1}.png`)} />
+              <img  style={{width: "12rem",height:"8rem", objectFit: "cover"}} src={require(`../../views/Media/assets/${i + 1}.png`)} />
             </a>
             </div>
           );
@@ -23,25 +23,25 @@ const CustomSlider = (props) => {
       };
 
       return (
-        <div style={{marginTop: "5rem"}}>
+        <div style={{marginTop: "5rem", marginRight: "1.5vw"}}>
         <Slider {...settings}>
         <div>
-            <img style={{margin: "auto", width: "auto", height: "21.625rem"}} src={require("../../views/Media/assets/1.png")} />
+            <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/1.png")} />
           </div>
           <div>
-            <img style={{margin: "auto", width: "30rem"}} src={require("../../views/Media/assets/2.png")} />
+            <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/2.png")} />
           </div>
           <div>
-             <img style={{margin: "auto", width: "30rem"}} src={require("../../views/Media/assets/3.png")} />
+             <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/3.png")} />
           </div>
           <div>
-            <img style={{margin: "auto", width: "30rem"}} src={require("../../views/Media/assets/4.png")} />
+            <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/4.png")} />
           </div>
           <div>
-            <img style={{margin: "auto", width: "30rem"}} src={require("../../views/Media/assets/5.png")} />
+            <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/5.png")} />
           </div>
           <div>
-            <img style={{margin: "auto", width: "30rem"}} src={require("../../views/Media/assets/6.png")} />
+            <img style={{margin: "auto", width: "auto", height: "40.833333333333332vw"}} src={require("../../views/Media/assets/6.png")} />
           </div>
 
         </Slider>
