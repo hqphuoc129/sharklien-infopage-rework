f4d4f52a489091831c825f9b942111f275313b3c
Eric Dang <ericdang2709@gmail.com>
2022-04-08 10:22:20 +0700

    fix-responsive

--- src/components/Description/Section_Paragraph.js
+++ src/components/Description/Section_Paragraph.js
@@ -1,6 +1,6 @@
 const Section_Paragraph = (props) => {
     return ( 
-    <p style = {{ textAlign: "justify", marginLeft: "10%", marginRight: "10%", marginBottom: "5%", fontSize: "1.4000000000000001vw", lineHeight: "1.5" } } > { props.content } </p>
+    <p className="para" style = {{ textAlign: "justify", marginLeft: "10%", marginRight: "10%", marginBottom: "5%", fontSize: "14pt", lineHeight: "1.5" } } > { props.content } </p>
     )
 }
 
