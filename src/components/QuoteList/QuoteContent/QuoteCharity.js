573b95a819144ec5ef5ff66e692c469163443938
Lephong2101 <letranphong1975@gmail.com>
2022-04-11 18:41:40 +0700

    fixQuoteCharity

--- src/components/QuoteList/QuoteContent/QuoteCharity.js
+++ src/components/QuoteList/QuoteContent/QuoteCharity.js
@@ -9,6 +9,32 @@ const QuoteCharity = () => {
         - “Liên & Dòng chảy nghĩa tình" lưu dấu một giai đoạn lịch sử bi tráng về tinh thần tương thân tương ái và kiên
         cường của dân tộc trong cơn dịch bệnh.
       </p>
+      <p>- Khi Sài Gòn phong thành, giữa hiểm nguy Covid-19, chúng tôi là “người được chọn".</p>
+      <p>
+        - Các tình nguyện viên chính là những Kim Đồng thời hiện đại. Nếu anh hùng ngày xưa phải đối mặt với bom rơi,
+        đạn lạc thì ngày nay, các bạn phải đối mặt với kẻ thù Covid-19 không rõ nhân dạng với tinh thần không lùi bước.{' '}
+      </p>
+      <p>
+        - Tôi đã khóc rất nhiều khi Việt Nam oằn mình trong cơn đại dịch. Tôi muốn cùng các con làm một điều gì đó. Thực
+        sự, làm từ thiện gian nan không thua gì kinh doanh.{' '}
+      </p>
+      <p>- Tôi muốn “Liên & Dòng chảy nghĩa tình” tiếp tục khơi mạch, lan tỏa và không có điểm dừng.</p>
+      <p>
+        - Trước nỗi đau của đồng bào, tôi không thể đứng yên được. Nó thôi thúc tôi phải làm điều gì đó ngay lập tức.
+      </p>
+      <p>
+        - Tôi chỉ góp vào những dòng tự sự. Còn lại là những chia sẻ nghĩa tình và tinh thần của tất cả mọi người, được
+        gói trọn trong cuốn sách.{' '}
+      </p>
+      <p>
+        - Những cây bút trẻ trong “Liên & Dòng chảy nghĩa tình” là những Kim Đồng thời hiện đại. Họ đã đi vào tâm bão
+        dịch bệnh để giúp người và ghi lại những câu chuyện thuộc về lịch sử.
+      </p>
+      <p>
+        - Chưa đầy 1 tuần, chương trình “Gởi tình Shark Liên - Gởi nghĩa đồng bào" đã được triển khai thần tốc. Nhờ vậy,
+        nhiều bà con được cứu giúp.{' '}
+      </p>
+      <p> </p>
     </div>
   );
 };
