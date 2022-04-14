37ca83005be5439b53b89d2f39a697c90f5a1d74
owentr1369 <tamvaa1306@gmail.com>
2022-04-14 12:03:22 +0700

    fix Testimonials Slider & assets

--- src/components/TestimonialsSlider/index.js
+++ src/components/TestimonialsSlider/index.js
@@ -1,61 +1,34 @@
-import Portrait from './assets/marko walde.png';
-import PortraitMobile from './assets/marko walde mobile.png';
 import { Carousel } from 'antd';
 import { CaretRightOutlined, CaretLeftOutlined } from '@ant-design/icons';
 import 'App.scss';
+import { people } from './people';
 
 function TestimonialsSlider() {
   return (
     <div className='slider_container'>
-      <h1 className='slider_header'>Để đi xa,
-chúng ta luôn cần có những người ủng hộ mình. </h1>
-      <Carousel autoplay arrows={true} nextArrow={<CaretRightOutlined/>} prevArrow={<CaretLeftOutlined/>}>
-        <div className='slider_content'>
-          <div className='slide_item-container'>
-            <div className='slide_item-content'>
-              <img className='portrait' src={Portrait}></img>
-              <div className='portrait-mobile-img'> 
-              <img className='portrait-mobile'src={PortraitMobile}></img>
+      <h1 className='slider_header'>Để đi xa, chúng ta luôn cần có những người ủng hộ mình. </h1>
+      <Carousel autoplay arrows={true} nextArrow={<CaretRightOutlined />} prevArrow={<CaretLeftOutlined />}>
+        {people.map((person) => (
+          <div key={person.id} className='slider_content'>
+            <div className='slide_item-container'>
+              <div className='slide_item-content'>
+                <div className='portrait-mobile-img'>
+                  <img className='portrait-mobile' src={person.avatar}></img>
+                </div>
+                <div className='slide_item-text'>
+                  {person.name}
+                  <br></br>
+                  {person.position}
+                </div>
+                <div className='slide_item-text-mobile'>
+                  <h2>{person.name}</h2>
+                  <p>{person.position}</p>
+                </div>
               </div>
-              <p className='slide_item-text'>
-                Ông Marko Walde, Trưởng Đại diện Phòng Thương mại và Công nghiệp Đức tại Việt Nam (AHK):
-              </p>
-              <div className='slide_item-text-mobile'>
-                <h2>Ông Marko Walde</h2>
-                <p>Trưởng Đại diện Phòng Thương mại và Công nghiệp Đức tại Việt Nam (AHK):</p>
-              </div>
+              <p className='slide_item-footer'>{person.text}</p>
             </div>
-            <p className='slide_item-footer'>
-              “Tôi đã làm việc ở Việt Nam rất lâu và từng gặp gỡ làm việc với bà Liên và gia đình bà nhiều lần. Thời
-              gian qua bà đã làm rất nhiều dự án tuyệt vời, bao gồm việc đầu tư qua Đức. Điều này chứng minh cho mối
-              quan hệ song phương bền chặt của chúng ta, không chỉ người Đức đầu tư vào Việt Nam mà người Việt Nam cũng
-              có thể tìm kiếm cơ hội đầu tư và làm điều gì đó cho nước Đức. Chính điều đó mới thực sự ý nghĩa”.
-            </p>
           </div>
-        </div>
-        <div className='slider_content'>
-          <div className='slide_item-container'>
-            <div className='slide_item-content'>
-              <img className='portrait' src={Portrait}></img>
-              <div className='portrait-mobile-img'> 
-              <img className='portrait-mobile'src={PortraitMobile}></img>
-              </div>
-              <p className='slide_item-text'>
-                Ông Marko Walde, Trưởng Đại diện Phòng Thương mại và Công nghiệp Đức tại Việt Nam (AHK):
-              </p>
-              <div className='slide_item-text-mobile'>
-                <h2>Ông Marko Walde</h2>
-                <p>Trưởng Đại diện Phòng Thương mại và Công nghiệp Đức tại Việt Nam (AHK):</p>
-              </div>
-            </div>
-            <p className='slide_item-footer'>
-              “Tôi đã làm việc ở Việt Nam rất lâu và từng gặp gỡ làm việc với bà Liên và gia đình bà nhiều lần. Thời
-              gian qua bà đã làm rất nhiều dự án tuyệt vời, bao gồm việc đầu tư qua Đức. Điều này chứng minh cho mối
-              quan hệ song phương bền chặt của chúng ta, không chỉ người Đức đầu tư vào Việt Nam mà người Việt Nam cũng
-              có thể tìm kiếm cơ hội đầu tư và làm điều gì đó cho nước Đức. Chính điều đó mới thực sự ý nghĩa”.
-            </p>
-          </div>
-        </div>
+        ))}
       </Carousel>
     </div>
   );
---
+++ src/components/TestimonialsSlider/people.js
@@ -0,0 +1,83 @@
+import Avt1 from './assets/NguyenBaNgoc.png';
+import Avt2 from './assets/NgocAnNhien.png';
+import Avt3 from './assets/CaoKhanhLinh.png';
+import Avt4 from './assets/TranTheTuyen.png';
+import Avt5 from './assets/NgoThiMyLoan.png';
+import Avt6 from './assets/JosefineWallat.png';
+import Avt7 from './assets/markowalde mobile.png';
+import Avt8 from './assets/RatubasiSuperMoloi.png';
+import Avt9 from './assets/KgomotsoRuthMagau.png';
+import Avt10 from './assets/Mpetjane KgaogeloLekgoro.png';
+
+export const people = [
+  {
+    id: 1,
+    avatar: Avt1,
+    name: 'Ông Nguyễn Bá Ngọc',
+    position: 'Giám đốc Công ty Mực nhảy Biển Đông',
+    text: '"Shark Liên là một nữ Doanh nhân tuyệt vời từ tâm và tài! Tôi đã chọn Cô là người để bản thân học hỏi và noi theo! Tôi tin rằng với tấm lòng nhân hậu và những việc làm đầy nhân văn của Cô sẽ làm thay đổi được nhiều điều tốt đẹp hơn cho đất nước!"',
+  },
+  {
+    id: 2,
+    avatar: Avt2,
+    name: 'Bà Ngọc An Nhiên',
+    position: 'Giám đốc Công ty TNHH Auto Đức Trí',
+    text: '"Lần đầu tiên tôi gặp Shark Liên qua zoom và được nghe Cô chia sẻ. Trong lòng tôi chợt thấy mình thật bé nhỏ trước người phụ nữ này. Tôi cảm nhận Cô Liên như một người mẹ có tình thương bao la, có tấm lòng Bồ tát. Gần gũi ấm áp. Không phải riêng tôi mà ai gặp Cô cũng muốn được ôm Cô để cảm nhận được sự ấm áp từ Cô tỏa ra, từ một người mẹ lan tỏa tình yêu thương cho các con của mình nguồn năng lượng an lành."',
+  },
+  {
+    id: 3,
+    avatar: Avt3,
+    name: 'Cao Khánh Linh',
+    position: 'Fan Club',
+    text: '"Ngoại Liên rất ấm áp, trái tim Ngoại bao la rộng lớn, luôn dang rộng vòng tay ôm lấy chúng tôi vào lòng. Mỗi cái ôm Ngoại trao là tình yêu thương đong đầy. Những câu chuyện Ngoại kể luôn dí dỏm, đời thường nhưng lại chân thực, thấu cảm, dễ hiểu, đọng lại những dư âm cho người đối diện. Mỗi phút bên Ngoại, tôi được tự do, được sống và cống hiến. Tất cả có ở LinhLinh là nguồn cảm hứng của sự sáng tạo nhận được từ Ngoại. Biết ơn Ngoại Liên đã mang đến cho con một người bạn tri kỷ là SÁCH LIÊN, để mỗi giây con được chuyển mình bước sang trang và lan tỏa mạnh mẽ đến cộng đồng yêu sách."',
+  },
+  {
+    id: 4,
+    avatar: Avt4,
+    name: 'Đại tá - Nhà báo Trần Thế Tuyển',
+    position: 'Nguyên Tổng Biên tập báo Sài Gòn Giải Phóng',
+    text: '"Thành đạt, nổi tiếng trên thương trường, Đỗ Liên còn nổi tiếng bởi những việc làm nghĩa tình, sẻ chia với cộng đồng; đặc biệt với gia đình có công với nước, gia đình liệt sĩ, thương binh."',
+  },
+  {
+    id: 5,
+    avatar: Avt5,
+    name: 'Ngô Mỹ Loan',
+    position: 'Fan club',
+    text: '"Nói về Madam Liên, tôi chỉ có từ “YÊU và NGƯỠNG MỘ” dành cho Ngoại. Xoay quanh những ngày tháng tôi đã tiếp xúc với Ngoại và tham gia những hoạt động của Ngoại, mỗi điều Ngoại làm đều đặt chữ T M lên hàng đầu và phải chỉnh chu từ những thứ nhỏ nhặt nhất. Tôi ngưỡng mộ Ngoại qua nhân cách, lối sống đơn giản, bình dị và cách đối nhân xử thế với mọi người. Và tôi cũng nhận lại từ Ngoại những bài học về tình người, nghĩa đồng bào trong cơn hoạn nạn, để trân quý cuộc sống mình đang có mặt."',
+  },
+  {
+    id: 6,
+    avatar: Avt6,
+    name: 'Tiến sĩ Josefine Wallat',
+    position: 'Tổng Lãnh sự Cộng hòa Liên bang Đức tại TP.HCM',
+    text: '"Khi cầm tấm bảng này trong tay, tôi muốn nói lòng biết ơn của người Đức tới gia đình Bà. Bà và gia đình đã có một hành động rất đẹp, thể hiện sự quan tâm tới người dân nước Đức, những con người đã và đang tiếp tục chống chọi với đại dịch và khủng hoảng. Tôi nghĩ việc này đã thể hiện được thông điệp của từ “tình bạn”. Mối quan hệ của chúng ta không chỉ diễn ra ở tầm quốc gia mà còn giữa những người dân. Chỉ trong những lúc khủng hoảng như thế này, bạn mới có thể nhận ra được một mối quan hệ thật sự là như thế nào!"',
+  },
+  {
+    id: 7,
+    avatar: Avt7,
+    name: 'Ông Marko Walde',
+    position: 'Trưởng Đại diện Phòng Thương mại và Công nghiệp Đức tại Việt Nam (AHK)',
+    text: '"Tôi đã làm việc ở Việt Nam rất lâu và từng gặp gỡ làm việc với bà Liên và gia đình bà nhiều lần. Thời gian qua bà đã làm rất nhiều dự án tuyệt vời, bao gồm việc đầu tư qua Đức. Điều này chứng minh cho mối quan hệ song phương bền chặt của chúng ta, không chỉ người Đức đầu tư vào Việt Nam mà người Việt Nam cũng có thể tìm kiếm cơ hội đầu tư và làm điều gì đó cho nước Đức. Chính điều đó mới thực sự ý nghĩa."',
+  },
+  {
+    id: 8,
+    avatar: Avt8,
+    name: 'Ngài Ratubasi Super Moloi',
+    position: 'Đại sứ đặc mệnh toàn quyền Cộng hòa Nam Phi tại Việt Nam nhiệm kỳ 2009-2013',
+    text: '“Madam Liên là người dám nghĩ dám làm, cầu toàn và là một người phụ nữ cuốn hút. Madam có khả năng thuyết phục bất cứ ai khi tiếp xúc và đem lòng yêu đất nước Nam Phi. Tôi thật sự ấn tượng với phương pháp điều hành các công việc kinh doanh cũng như rất nhiều giải thưởng danh giá mà Bà được trao tặng. Chính phủ Nam Phi sẽ mãi cảm kích và biết ơn khi có được một Lãnh sự danh dự như Bà. Cá nhân tôi thực sự muốn chúc mừng và mong bà biết rằng chúng tôi luôn rất tự hào về Bà.”',
+  },
+  {
+    id: 9,
+    avatar: Avt9,
+    name: 'Bà Kgomotso Ruth Magau',
+    position: 'Cựu Đại sứ đặc mệnh toàn quyền Nam Phi tại Việt Nam (nhiệm kỳ 2013-2017)',
+    text: '“Tôi muốn nhấn mạnh sự đóng góp to lớn của Madam Liên trong việc tăng cường mối quan hệ song phương giữa Nam Phi và Việt Nam. Bà luôn sẵn lòng hỗ trợ các công việc của Đại sứ quán tại Hà Nội thông qua việc đảm bảo Đại sứ quán luôn được thông báo kịp thời các sự vụ cần được đặc biệt quan tâm, đồng thời thường xuyên hỗ trợ đội ngũ của Đại sứ quán trong quá trình giải quyết những sự vụ đó.”',
+  },
+  {
+    id: 10,
+    avatar: Avt10,
+    name: 'Ngài Mpetjane Kgaogelo Lekgoro',
+    position: 'Đương kim Đại sứ đặc mệnh toàn quyền Cộng hòa Nam Phi tại Việt Nam',
+    text: '“Với những đóng góp trong việc thúc đẩy mối quan hệ hợp tác giữa Cộng hòa Nam Phi và Việt Nam thông qua việc khởi xướng và tổ chức nhiều chương trình giao lưu, các hoạt động hỗ trợ, kết nối và xúc tiến thương mại giữa hai nước suốt 10 năm qua, Madam Đỗ Thị Kim Liên là Đại sứ hình ảnh của đất nước Nam Phi tại Việt Nam và Việt Nam tại Nam Phi.”',
+  },
+];
