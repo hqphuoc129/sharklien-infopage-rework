e9e07a315e6b34def0a7d438c27b823ec1b5ca95
Eric Dang <ericdang2709@gmail.com>
2022-04-17 11:34:01 +0700

    commit-footerlogo-link and title

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,24 @@
 import FooterLogo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+import {webContext} from '../../App';
+import React, {useContext} from 'react'; 
 
 function Footer() {
+  const {isActive, setIsActive} = useContext(webContext)
   return (
     <div className="footer">
-      <div  className="footer_logo">
+      <Link
+        to='/'
+        className={`footer_logo ` + isActive === 'home' ? 'nav active' : ''}
+        onClick={() => {
+          setIsActive('home');
+          window.scrollTo(0,0);
+        }}
+      >
         <div className ='footer_logo-wrapper'> 
           <img className="footer-img" src = {FooterLogo}/> 
         </div> 
-      </div>
+      </Link>
         <div className='footer_text-wrapper'>
         <p className="footer_text">
         Đây là website giới thiệu về tôi - Đỗ Liên cùng hệ sinh thái kinh doanh, các hoạt động thiện nguyện và định hướng hỗ trợ cộng đồng của tôi
