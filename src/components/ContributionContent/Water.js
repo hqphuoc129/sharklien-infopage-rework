d2ffdfc2719556cb632bd90647f51bc971b1cbdb
Eric Dang <ericdang2709@gmail.com>
2022-04-19 21:02:06 +0700

    change language

--- src/components/ContributionContent/Water.js
+++ src/components/ContributionContent/Water.js
@@ -4,6 +4,10 @@ import nuocsach1 from './assets/nuocsach1.jpg';
 import nuocsach2 from './assets/nuocsach2.jpg';
 import nuocsach3 from './assets/nuocsach3.jpg';
 import nuocsach4 from './assets/nuocsach4.jpg';
+import SelectLang from 'components/Language/selectLang';
+import VietNam from '../../lang/vn.json';
+
+import {FormattedMessage, FormattedDate} from 'react-intl';
 
 const vid_nuocsach = [
   {
@@ -30,34 +34,27 @@ const Water = () => {
     <>
     <div className='water-content insurance-content-mobile'>
       <p>
-        “Ở đâu có nước, ở đó có sự sống”. Với ý nghĩa đó, tôi đã thành lập Tập đoàn AquaOne - chuyên cung cấp và xử lý
-        nước sạch phục vụ cho hoạt động sinh hoạt của người dân. Với các công trình nước sạch dân sinh, AquaOne đảm bảo
-        cân đối các ưu tiên theo thứ tự: Chất lượng nước tuyệt đối an toàn, vệ sinh, sạch và không có nguy cơ gây bệnh
-        trước mắt cũng như lâu dài.
+        <FormattedMessage id="para1" defaultMessage={VietNam['para1']}/>
       </p>
       <p>
-        Về mặt xã hội: AquaOne góp phần nâng cao sức khỏe người dân, đặc biệt là trẻ em, người già và phụ nữ. Dùng nước
-        sạch giúp giảm bệnh tật gây ra từ nguồn nước không hợp vệ sinh. Các Nhà máy của AquaOne cũng tạo thêm công ăn
-        việc làm cho người dân.
+      <FormattedMessage id="para2" defaultMessage={VietNam['para2']}/>
       </p>
       <p>
-        Về kinh tế: những dịch vụ của nước không thể lượng giá được nhưng lại có giá trị kinh tế lớn. AquaOne góp phần
-        đa dạng hóa các thành phần kinh tế trong sản xuất và kinh doanh nước sạch, thúc đẩy quá trình đô thị hóa tại các
-        địa phương, đồng thời thu hút các nhà đầu tư.
+      <FormattedMessage id="para3" defaultMessage={VietNam['para3']}/>
       </p>
       <p>
-        Hiện tại, Tập đoàn AquaOne là Chủ đầu tư của các dự án lớn: Nhà máy nước mặt Sông Hậu (Tỉnh Hậu Giang), Nhà máy
-        nước mặt Sông Đuống (TP.Hà Nội), Nhà máy nước mặt Xuân Mai – Hòa Bình (Hòa Bình), Nhà máy nước mặt Sông Vàm Cỏ
-        Đông (Long An).
+      <FormattedMessage id="para4" defaultMessage={VietNam['para4']}/>
       </p>
       <p>
-        Với nguồn nước khan hiếm hiện nay, tôi mong người dân hãy sử dụng nước một cách phù hợp, tránh lãng phí và có ý
-        thức hơn trong việc bảo vệ môi trường, tránh làm ô nhiễm nguồn nước.
+      <FormattedMessage id="para5" defaultMessage={VietNam['para5']}/>
       </p>
       <p>
-        Tham quan dự án nước sạch của tôi: <a className="link-click" href="https://aquaone.vn" >aquaone.vn</a>
+      <FormattedMessage id="para6" defaultMessage={VietNam['para6']}/><a className="link-click" href="https://aquaone.vn" >        <FormattedMessage id="web-link" defaultMessage={VietNam['web-link']}/></a>
       </p>
+      <p>
+      <FormattedMessage id="vid-section-title" defaultMessage={VietNam['vid-section-title']}/></p>
     </div>
+    <SelectLang/>
     <CauouselSliderContainer vidlist= {vid_nuocsach} linkClass={"carousel-media"} title={"Các video liên quan"} totalNum ={3}/>
     </> 
   );
