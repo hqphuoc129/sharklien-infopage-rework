e4baa720cdd6011e1042b556691fb7f5f03712c4
Eric Dang <ericdang2709@gmail.com>
2022-04-23 23:03:55 +0700

    commit fixing lang

--- src/components/Language/selectLang.js
+++ src/components/Language/selectLang.js
@@ -1,13 +1,28 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Context} from 'components/Language';
-import {CustomSelect} from 'react-select-with-icon'
+import { NavDropdown } from 'react-bootstrap';
 
 import {FiGlobe} from 'react-icons/fi';
 const SelectLang = () => {
+    const [isSelected, setIsSelected] = useState("")
     const context = useContext(Context)
     return (
         <>
-        <select value={context.locale} onChange={context.selectLanguage}>
+        <NavDropdown
+            title={
+                <FiGlobe/>
+            }
+
+            value={context.locale} 
+            onChange={context.selectLanguage}
+
+            onSelect={context.selectLanguage}
+
+        >
+            <NavDropdown.Item value="vn" eventKey={"en"}>VN</NavDropdown.Item>
+            <NavDropdown.Item value="en"  onClick={() => (setIsSelected("en"))}>Eng</NavDropdown.Item>
+        </NavDropdown>
+        <select value={context.locale} title={<FiGlobe/>} onChange={context.selectLanguage}>
             <option value="vn">Viet Nam</option>
             <option value="en">English</option>
         </select>
