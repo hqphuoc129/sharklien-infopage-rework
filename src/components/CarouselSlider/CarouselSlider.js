ea46d7a0e898bdb399ac95ba9121d09ad58c396a
hoaiphuc2000 <lephanhoaiphuc@gmail.com>
2022-04-24 16:58:38 +0700

    fix pics carousel slider index

--- src/components/CarouselSlider/CarouselSlider.js
+++ src/components/CarouselSlider/CarouselSlider.js
@@ -6,6 +6,8 @@ import CarouselSliderImg2 from "components/CarouselSlider/assets/CarouselSliderI
 import CarouselSliderImg1en from "components/CarouselSlider/assets/CarouselSliderImg1-en.jpg";
 import CarouselSliderImg2en from "components/CarouselSlider/assets/CarouselSliderImg2-en.jpg";
 
+import CarouselSliderMobile from "components/CarouselSlider/assets/carousel-mobile.jpg"
+import CarouselSliderMobileen from "components/CarouselSlider/assets/carousel-mobile-en.jpg"
 import CarouselBackgroundImg from "components/CarouselSlider/assets/carousel-mobile.jpg";
 import React, {useEffect,useState,useContext} from 'react';
 import {Context} from 'components/Language';
@@ -13,15 +15,18 @@ const CarouselSlider = () => {
   
   const context = useContext(Context);
   let LIST_IMG = [CarouselSliderImg1,CarouselSliderImg2];
+  let LIST_IMG_MOBILE = CarouselSliderMobile
   if (context.locale === "vn"){
     LIST_IMG = [CarouselSliderImg1,CarouselSliderImg2];
+    LIST_IMG_MOBILE = CarouselSliderMobile
   }
   else if(context.locale === "en"){
     LIST_IMG = [CarouselSliderImg1en,CarouselSliderImg2en];
+    LIST_IMG_MOBILE = CarouselSliderMobileen
   }
   return (
     <div className="carousel-mobile">
-      <img src={CarouselBackgroundImg} className="carousel-mobile-img"/>
+      <img src={LIST_IMG_MOBILE} className="carousel-mobile-img"/>
       <Carousel autoplay className="carousel-home">
         {LIST_IMG.map((item, idx) => (
           <div className="carousel-background">
