28d90931b9b47ec46d1b21aea6919cf6b2c606da
Eric Dang <ericdang2709@gmail.com>
2022-05-01 21:11:56 +0700

    fetch-test

--- src/components/Language/selectLang.js
+++ src/components/Language/selectLang.js
@@ -1,6 +1,5 @@
 import React, {useContext, useState} from 'react';
 import {Context} from 'components/Language';
-import { NavDropdown } from 'react-bootstrap';
 import "antd/dist/antd.css"; 
 import {Select} from "antd";
 import {FiGlobe} from 'react-icons/fi';
@@ -12,24 +11,20 @@ const Options = [
     {label: "ENG", value: "en"},
 ]
 
+
 const {Option} = Select;
 
 const SelectLang = () => {
-    const [isSelected, setIsSelected] = useState("vn")
+
+    const toggle = () => {setisOpen(!isOpen)} 
+    const [isOpen, setisOpen] = useState(false)
     const context = useContext(Context)
     return (
         <>
-
-        <Select
-            suffixIcon = {<FiGlobe/>}
-            value={context.locale}
-            onChange={(val) => context.selectLanguage(val)}
-            labelInValue
-            style={{display: "none"}}
-        >
-            <Option value="vn"> Tiếng Việt</Option>
-            <Option value="en"> Tiếng Anh</Option>
-        </Select>
+        <div style={{height: 'auto', width: '4rem', display:"none"}}
+            onClick = {()=> (toggle())}
+            >Clickme</div>
+        { isOpen && (<div style={{height: 'auto', width: '7rem', backgroundColor: "black", position:"absolute", top: "5rem"}} >Opened</div>)} 
         <select value={context.locale} onChange={context.selectLanguage} className="btn-change-lang">
             {console.log(VietNam['lang-vie'])}
            <FormattedMessage id="lang-vie" defaultMessage={VietNam['lang-vie']}>{(text) => <option value="vn">{text}</option>}</FormattedMessage>            
