1b95035bfbe0f4808c06764f711eb7972801a18a
Eric Dang <ericdang2709@gmail.com>
2022-05-04 18:16:21 +0700

    slider-error

--- src/components/Media/GalleryImage.js
+++ src/components/Media/GalleryImage.js
@@ -1,11 +1,27 @@
 import { SRLWrapper } from "simple-react-lightbox";
 
+import React, {useEffect} from "react";
+
 const GalleryImage = (props) => {
+    const options = {
+    thumbnails: {
+        showThumbnails: true,
+    }
+}
+
+    useEffect(() => {
+
+    },[props.data])
     return (
-        <SRLWrapper>
-            {props.data.map((link) => (
-              <img src={link} alt={link} />
-            ))}
+        <SRLWrapper options ={options}>
+            {
+            props.data.data?.filter(
+                (link) => 
+             link.collectionName === props.collectionName
+            ).map(filtereditem => filtereditem.mediaList.map(item => (
+                <img src={item} alt={item}/>
+            )))
+            }
         </SRLWrapper>
     )
 }
